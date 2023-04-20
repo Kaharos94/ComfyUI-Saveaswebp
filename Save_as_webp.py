87ba2ac69dccfe45de5a0ebe02d3ab224c81dac6
wyrde <wyrde@users.noreply.github.com>
2023-04-20 09:24:11 -0400

    Update Save_as_webp.py
    added attribution, source repo, and summary

--- Save_as_webp.py
+++ Save_as_webp.py
@@ -6,7 +6,9 @@ import folder_paths
 import os
 import json
 
-
+// by Kaharos94
+// https://github.com/Kaharos94/ComfyUI-Saveaswebp
+// comyUI node to save an image in webp format
 
 class Save_as_webp:
     def __init__(self):
